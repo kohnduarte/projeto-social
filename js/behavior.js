926b37369da48139a25b530f92e47a4c196284b6
Erick Kohn <133143910+kohnduarte@users.noreply.github.com>
2023-12-16 12:37:40 -0300

    Update behavior.js

--- js/behavior.js
+++ js/behavior.js
@@ -1,5 +1,8 @@
 function horaAgora() {
     var P = window.document.getElementById('hora')
+    var h = new Date().getHours().toString().padStart(2, '0')
+    var m = String(new Date().getMinutes()).padStart(2, '0')
+    P.innerText = `${h}:${m}`
     setInterval(horaAtual, 1000)
     function horaAtual() {
         var h = new Date().getHours().toString().padStart(2, '0')
